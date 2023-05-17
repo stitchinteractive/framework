458afc4c605c14fc3ca99985caca9f87674a2134
stitchinteractiveivan <creatives@stitchinteractive.com>
2023-05-17 09:43:24 +0800

    Commit Column Grid component

---
+++ src/pages/columns.js
@@ -0,0 +1,12 @@
+import React from "react"
+import MyComponent from "../components/columns"
+
+const MyPage = () => {
+  return (
+    <div>
+      <MyComponent columnCount={5} />
+    </div>
+  )
+}
+
+export default MyPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,9 @@ const IndexPage = () => (
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
           <h1 className="fw-light">Hello world ! </h1>
-          <p className="lead text-muted"> Welcome to this Boostrap 5 Gatsby Starter hi</p>
+          <p className="lead text-muted">
+            Welcome to this Boostrap 5 Gatsby Starter hi
+          </p>
           <StaticImage
             src="../images/gatsby-astronaut.png"
             width={300}
@@ -20,12 +22,15 @@ const IndexPage = () => (
             alt="A Gatsby astronaut"
             className="img-fluid"
           />
-
         </div>
       </div>
       <div className="row">
-        <Link to="/about/" className="btn btn-primary my-2">About</Link>
-        <Link to="/page-2/" className="btn btn-secondary my-2">Go to page 2</Link>
+        <Link to="/about/" className="btn btn-primary my-2">
+          About
+        </Link>
+        <Link to="/page-2/" className="btn btn-secondary my-2">
+          Go to page 2
+        </Link>
       </div>
     </section>
   </Layout>
@@ -33,6 +38,4 @@ const IndexPage = () => (
 
 export default IndexPage
 
-export const Head = () => (
-    <Seo />
-)
+export const Head = () => <Seo />
