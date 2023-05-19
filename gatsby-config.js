9bfdf81df84ebe324ffc60d2a81a8f765ef2ac38
stitchinteractiveivan <creatives@stitchinteractive.com>
2023-05-19 14:12:16 +0800

    installed gatsby mui

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `stitch interactive framework`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
